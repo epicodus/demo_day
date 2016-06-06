29cbc5fba7aca568d4f29e02c259693128a68625
Christopher Joy-Webb <chris@joywebb.com>
2016-06-06 11:31:59 -0700

    add new spreadsheet url and update index.html to match new questions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() { init() };
 
-var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1mOpVH9eJM-EN7he0tQIjNPeceJGncwxUftdYbvG92oo/pubhtml?gid=1445492928&single=true';
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1NWkttEaz3WkDB_nLQEwkKH6LJP3RE6f97qpCjUwtDbE/pubhtml';
 
 function init() {
   Tabletop.init( {
