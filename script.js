dc31179cd4b4c11cbd99609518cb373db4b63f6a
Mike Goren <mike@mortalwombat.net>
2016-11-15 13:17:05 -0800

    update for Nov 2016 class

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onload = function() { init() };
 
 // var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1hMGMJriWaavla_2Pf-NfuL0HRTfhKGkyKLYWZAsrjWI/pubhtml';
-var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/15W-tVoLLIYyaqRwBA9nmkC3_9-Hx_X9SgE-cxGbjdXE/pubhtml?gid=1862368927&single=true';
+// var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/15W-tVoLLIYyaqRwBA9nmkC3_9-Hx_X9SgE-cxGbjdXE/pubhtml?gid=1862368927&single=true';
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1Cuu3yCxdJicN-YSo2RJB9lzMyDefNcfhJKiALMebPhw/pubhtml?gid=305673636&single=true';
 
 function init() {
   Tabletop.init( {
@@ -25,13 +26,11 @@ function drawTable(sheets, tabletop) {
     var student = students[i];
     var html_row = $("<tr></tr>");
     for(var j = 0; j < column_names.length; j++) {
-      if(j===0 || j===2) { // TEMPORARILY SHOW ONLY NAME & LINKEDIN
-        var column_name = column_names[j];
-        if (student[column_name].indexOf('http') > -1) {
-          $("<td><a target='_blank' href='" + student[column_name] + "'>" + student[column_name] + "</a></td>").appendTo(html_row);
-        } else {
-          $("<td></td>").text(student[column_name]).appendTo(html_row);
-        }
+      var column_name = column_names[j];
+      if (student[column_name].indexOf('http') > -1) {
+        $("<td><a target='_blank' href='" + student[column_name] + "'>" + student[column_name] + "</a></td>").appendTo(html_row);
+      } else {
+        $("<td></td>").text(student[column_name]).appendTo(html_row);
       }
     }
     tbody.append(html_row);
