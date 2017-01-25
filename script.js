57b85d385acea7e9e228407db9c082f31cf1f9dc
Mike Goren <mike@mortalwombat.net>
2017-01-25 15:08:08 -0800

    update jan 2017

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ window.onload = function() { init() };
 
 // var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1hMGMJriWaavla_2Pf-NfuL0HRTfhKGkyKLYWZAsrjWI/pubhtml';
 // var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/15W-tVoLLIYyaqRwBA9nmkC3_9-Hx_X9SgE-cxGbjdXE/pubhtml?gid=1862368927&single=true';
-var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1Cuu3yCxdJicN-YSo2RJB9lzMyDefNcfhJKiALMebPhw/pubhtml?gid=305673636&single=true';
+// var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1Cuu3yCxdJicN-YSo2RJB9lzMyDefNcfhJKiALMebPhw/pubhtml?gid=305673636&single=true';
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/14rhSKJaKOQB9NWp4VAahnkgBl_tBDCbnAsyOth4-5Lo/pubhtml';
 
 function init() {
   Tabletop.init( {
@@ -16,6 +17,7 @@ function drawTable(sheets, tabletop) {
   var sheet = tabletop.sheets('Form Responses 1');
 
   sheet.column_names.shift();
+  sheet.column_names.shift();
 
   var column_names = sheet.column_names,
       students = sheet.elements,
@@ -25,7 +27,7 @@ function drawTable(sheets, tabletop) {
   for(var i = 0; i < students.length; i++) {
     var student = students[i];
     var html_row = $("<tr></tr>");
-    for(var j = 0; j < column_names.length; j++) {
+    for(var j = 0; j < 6; j++) {
       var column_name = column_names[j];
       if (student[column_name].indexOf('http') > -1) {
         $("<td><a target='_blank' href='" + student[column_name] + "'>" + student[column_name] + "</a></td>").appendTo(html_row);
